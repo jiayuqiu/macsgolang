db5d9712560614ba2cc8494cabcb096a54f804b4
macs <sohu.321@qq.com>
2022-06-01 19:25:52 +0800

    ch4 map code

---
+++ src/bible/ch4/map.go
@@ -0,0 +1,72 @@
+package ch4
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"unicode"
+	"unicode/utf8"
+)
+
+func MapDemo1() {
+	var names []string
+	ages := map[string]int{
+		"alice":   31,
+		"charlie": 34,
+		"macs":    28,
+		"lynn":    18,
+	}
+
+	for name := range ages {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	for i, name := range names {
+		fmt.Printf("%s\t%d\n", name, ages[name])
+		fmt.Println(i)
+	}
+}
+
+func Practice48CharCount() {
+	counts := make(map[rune]int)    // counts of Unicode characters
+	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
+	invalid := 0                    // count of invalid UTF-8 characters
+
+	in := bufio.NewReader(os.Stdin)
+	cc := 0
+	for {
+		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+		cc++
+		if cc > 50 {
+			break
+		}
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"awesomeProject/src/bible/ch1"
-	"awesomeProject/src/bible/ch4"
-	"fmt"
+	"macagolang/src/bible/ch1"
+	"macagolang/src/bible/ch4"
 )
 
-func main(){
+func main() {
 	// 调用自定义函数
 	ch1.PrintHello()
 
@@ -28,20 +27,24 @@ func main(){
 	//ch4.TestArray()
 
 	// slice
-	var aPtr *[5]int
-	var a = [5]int{2,4,6,8,10}
-	aPtr = &a  // 生成数组指针的方法
-	ch4.Practice43Reverse(aPtr)
-	fmt.Println(a)
+	//var aPtr *[5]int
+	//var a = [5]int{2,4,6,8,10}
+	//aPtr = &a  // 生成数组指针的方法
+	//ch4.Practice43Reverse(aPtr)
+	//fmt.Println(a)
+	//
+	//var s = []string{"zero", "one", "one", "two", "three"}
+	//var s2 []string
+	//s2 = ch4.Practice45Dup(s)
+	//fmt.Println(s2)
+	//
+	//s46 := "hello      世界 46"
+	//b46 := []byte(s46)
+	//fmt.Println(s46)
+	//b46Res := ch4.Practice46(b46)
+	//fmt.Println(string(b46Res))
 
-	var s = []string{"zero", "one", "one", "two", "three"}
-	var s2 []string
-	s2 = ch4.Practice45Dup(s)
-	fmt.Println(s2)
-
-	s46 := "hello      世界 46"
-	b46 := []byte(s46)
-	fmt.Println(s46)
-	b46Res := ch4.Practice46(b46)
-	fmt.Println(string(b46Res))
+	// map
+	//ch4.Practice48CharCount()
+	ch4.MapDemo1()
 }

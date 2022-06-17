ecefbe2383d64dd638f01cec59276347f2da5624
qiujiayu <sohu.321@qq.com>
2022-06-17 09:06:42 +0800

    update: 添加搜索comic信息搜索功能

--- src/bible/ch4/412_searchComic.go
+++ src/bible/ch4/412_searchComic.go
@@ -4,25 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 type XKCDresponse struct {
-	Month string
-	Num int64
-	Link string
-	Year string
-	News string
-	SafeTitle string
+	Month      string
+	Num        int64
+	Link       string
+	Year       string
+	News       string
+	SafeTitle  string
 	Transcript string
-	Alt string
-	Img string
-	Title string
-	Day string
+	Alt        string
+	Img        string
+	Title      string
+	Day        string
 }
 
-func searchComic(comicID int)(*XKCDresponse, error){
+func searchComic(comicID int) (*XKCDresponse, error) {
 	//q := url.QueryEscape(strings.Join(terms, " "))
 	resp, err := http.Get(fmt.Sprintf("https://xkcd.com/%d/info.0.json", comicID))
 	if err != nil {
@@ -38,40 +37,69 @@ func searchComic(comicID int)(*XKCDresponse, error){
 		resp.Body.Close()
 		return nil, err
 	}
-	//fmt.Println(result)
 	resp.Body.Close()
 	return &result, nil
 }
 
-
-func Practice412(){
+func Practice412() {
 	//The popular web comic xkcd has a JSON interface. For example,
 	//a request to http://xkcd.com/571/info.0.json produces a detailed description of comic 571,
 	//one of many favorites. Download each URL (once!) and build an offline index.
 	//Write a tool xkcd that, using this index,
 	//prints the URL and transcript of each comic that matches a search term provided on the command line.
 	//TODO: 可以添加一个函数，以title作为key进行检索。
-	for i := 571; i <= 600; i++ {
-		result, err := searchComic(i)
+	// for i := 571; i <= 575; i++ {
+	// 	result, err := searchComic(i)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	comicTitle := result.Title
+	// 	dataString, err := json.MarshalIndent(result, "", "    ")
+	// 	if err != nil {
+	// 		log.Fatalf("JSON MarshalIndent failed: %s", err)
+	// 	}
+	// 	content := []byte(dataString)
+	// 	err = ioutil.WriteFile(
+	// 		fmt.Sprintf("/Users/qiujiayu/GolandProjects/macsgolang/utils/xkcdComic/%v.json", comicTitle),
+	// 		content,
+	// 		0644,
+	// 	)
+	// 	if err != nil {
+	// 		fmt.Println(">>>>>>>>>>>>>>>>>>> error start >>>>>>>>>>>>>>>>>")
+	// 		fmt.Println(err)
+	// 		fmt.Println(">>>>>>>>>>>>>>>>>>>  error end  >>>>>>>>>>>>>>>>>")
+	// 	}
+	// }
+
+	// 读取文件夹内的所有内容，并生成hash表
+	// key文件title, value为json对应struct
+	folderPath := "/Users/qiujiayu/GolandProjects/macsgolang/utils/xkcdComic/"
+	files, _ := ioutil.ReadDir(folderPath)
+	localComicInfo := make(map[string]*XKCDresponse)
+	for _, f := range files {
+		// fmt.Println(f.Name())
+		// 读取json文件，并将文件中的内容转换为struct
+		fmt.Println(folderPath + f.Name())
+		strBytes, err := ioutil.ReadFile(folderPath + f.Name())
+		// fmt.Printf("%v %T", strBytes, strBytes)
+
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println("读取json文件失败", err)
+			fmt.Println(f.Name())
+			return
 		}
-		fmt.Printf("%v \n", result)
-		comicTitle := result.Title
-		dataString, err := json.MarshalIndent(result, "", "    ")
+
+		// var res XKCDresponse
+		res := &XKCDresponse{} // 此处初始化需要用指针
+
+		err = json.Unmarshal(strBytes, res)
 		if err != nil {
-			log.Fatalf("JSON MarshalIndent failed: %s", err)
+			fmt.Println("解析数据失败", err)
+			return
 		}
-		content := []byte(dataString)
-		err = ioutil.WriteFile(
-				fmt.Sprintf("/Users/qiujiayu/GolandProjects/macsgolang/utils/xkcdComic/%v.json", comicTitle),
-				content,
-				0644,
-			)
-		if err != nil {
-			fmt.Println(">>>>>>>>>>>>>>>>>>> error start >>>>>>>>>>>>>>>>>")
-			fmt.Println(err)
-			fmt.Println(">>>>>>>>>>>>>>>>>>>  error end  >>>>>>>>>>>>>>>>>")
-		}
+		localComicInfo[f.Name()] = res
 	}
+
+	fmt.Println(localComicInfo["Addiction.json"])
+	fmt.Println(localComicInfo["Addiction.json"].Title)
 }
